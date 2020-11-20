b2c0180dc54b5845ce866f364049f41cfccc107b
wyq977 <wangyq977@gmail.com>
2020-11-20 18:19:06 +0100

    v0.1.1

--- lbibhelper/__init__.py
+++ lbibhelper/__init__.py
@@ -7,4 +7,4 @@ from .report_processor import *
 __all__ = ["core", "fit", "report_processor"]
 __author__ = """Yongqi Wang"""
 __email__ = "wangyq977@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
